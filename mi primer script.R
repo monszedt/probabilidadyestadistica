abd3c2bdd0222cba3b967d6336bd4903f786c7a6
monszedt <monsszedt@gmail.com>
2020-09-30 15:28:56 -0500

    Este es mi primer script Me senti muy estresada y confundida

---
+++ mi primer script.R
@@ -0,0 +1,8 @@
+
+#vamos a crear un vector que contenga 5 variables cuantitativas discretas
+#contenidas en una variable llamada muestra 
+
+muestra<- c(10,35,20,18,15)
+
+#vamos a calcular la media 
+mean(muestra)
