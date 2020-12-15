adf24a9b0205e79f70c72ee1acf074f1f52252db
monszedt <72088691+monszedt@users.noreply.github.com>
2020-12-15 00:34:07 -0600

    3er examen parcial parte I
    3er examen parcial parte I

---
+++ 3er examen parcial parte I.R
@@ -0,0 +1,25 @@
+#Ejercicios para 3er examen parcia: Parte I
+
+#------Ejercicios de distribución binomial-----------
+
+
+#Ejercicio 1 
+
+sum(dbinom(2:3,12,0.2))
+
+#Ejercicio 2 
+
+dbinom(1,size = 25,prob = 0.005)
+
+
+
+#-----Ejercicios de distribución de Poisson----------
+
+#Ejercicio 1
+dpois(34,5)
+
+#Ejercicio 2
+dpois(60,5)
+
+#Ejercicio 3
+dpois(120,5)
